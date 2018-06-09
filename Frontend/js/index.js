61117cffb96d06321e0df075f28ae0cf590f4ec8
OscarVinueza <os.vinueza94@gmail.com>
2018-06-09 13:03:42 -0500

    Primera subida de proyecto final base de datos

---
+++ Frontend/js/index.js
@@ -0,0 +1,42 @@
+$(function(){
+  var l = new Login();
+})
+
+
+class Login {
+  constructor() {
+    this.submitEvent()
+  }
+
+  submitEvent(){
+    $('form').submit((event)=>{
+      event.preventDefault()
+      this.sendForm()
+    })
+  }
+
+  sendForm(){
+    let form_data = new FormData();
+    form_data.append('username', $('#user').val())
+    form_data.append('password', $('#password').val())
+    $.ajax({
+      url: '../Backend/check_login.php',
+      dataType: "json",
+      cache: false,
+      processData: false,
+      contentType: false,
+      data: form_data,
+      type: 'POST',
+      success: function(php_response){
+        if (php_response == 1) {
+          window.location.href = 'main.html';
+        }else {
+          alert("usuario o contraseña incorrectos");
+        }
+      },
+      error: function(){
+        alert("error en la comunicación con el servidor");
+      }
+    })
+  }
+}
